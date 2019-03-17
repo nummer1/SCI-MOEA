293d05d44f110530fa907cc4ac9990b35e680cfd
nummer1 <AnonymNN2@gmail.com>
2019-03-17 20:23:12 +0100

    moved to shutdown

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,20 @@
-import kotlin.math.roundToInt
-
 // http://shiralab.ynu.ac.jp/data/paper/cec09_shirakawa.pdf
 // http://www.academia.edu/6394976/Multi-Objective_Evolutionary_Clustering_using_Variable-Length_Real_Jumping_Genes_Genetic_Algorithm
 // https://github.com/simjohan/bio-ai-project2/blob/master/pdf/978-3-319-71928-3_17.pdf
 
 fun main(args: Array<String>) {
     val minSegmentSize = 300
-    val genCount = 30
+    val genCount = 2
     val popSize = 20
     val mutationRate = 0.3
     val crossoverRate = 1.0
-    val minSegmentCount = 4
-    val maxSegmentCount = 16
+    val minSegmentCount = 3
+    val maxSegmentCount = 40
 
-    val problem = Problem("353013/Test image.jpg", minSegmentSize)
-
+    val problem = Problem("216066/Test image.jpg", minSegmentSize)
     val nsga2 = NSGA2(problem, genCount, popSize, mutationRate, minSegmentCount, maxSegmentCount)
     nsga2.run()
-    var i = 0
-    for (pop in nsga2.getNondominatedPopulation()) {
-        val name = "pop_${i}segments=${pop.segmentClass.partitions!!.size}_OD=${pop.overallDeviation.roundToInt()}_CM=${pop.connectivityMeasure.roundToInt()}_EV=${pop.edgeValue.roundToInt()}.png"
-        problem.drawOnBlank(pop, "GT$name")
-        problem.drawOnImage(pop, name)
-        i += 1
-    }
+    nsga2.shutdown()
 
 //    val ga = GA(problem, genCount, popSize, mutationRate, maxSegmentCount, minSegmentCount)
 //    ga.run()
@@ -31,6 +22,7 @@ fun main(args: Array<String>) {
 //    val name = "pop_best_segments=${pop.segmentClass.partitions!!.size}_OD=${pop.overallDeviation.roundToInt()}_CM=${pop.connectivityMeasure.roundToInt()}_EV=${pop.edgeValue.roundToInt()}.png"
 //    problem.drawOnBlank(pop, "GT$name")
 //    problem.drawOnImage(pop, name)
+
 
 
 //    val dir = Direction(problem.width, problem.height)
